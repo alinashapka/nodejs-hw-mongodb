7b91f23ec4262f62c1a6090737cd6535466c2177
alina <alinkash2811@gmail.com>
2025-07-23 12:07:39 +0200

    fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -50,7 +50,7 @@ export async function deleteContactController(req, res) {
     throw new createHttpError.NotFound('Contact not found');
   }
 
-  res.json({ status: 204 });
+  res.status(204).send();
 }
 
 export async function updateContactController(req, res) {
